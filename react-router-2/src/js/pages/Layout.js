90cb55e02d12216dd11043db885294929d67606d
creightonja <creightonja@gmail.com>
2016-05-11 11:07:37 +0900

    remake killernews.net for react page

--- react-router-2/src/js/pages/Layout.js
+++ react-router-2/src/js/pages/Layout.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { Link } from "react-router";
 
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
+
 export default class Layout extends React.Component {
-	navigate() {
-		this.props.history.replaceState(null, "/");
-	}
-	render() {
-		return (
-			<div>
-				<h1>Title goes here</h1>
-				{this.props.children}
-				<Link class="btn btn-primary" to="archives">archives</Link>
-				<Link class="btn btn-danger" to="settings">settings</Link>
-				<button class="btn btn-primary" onClick={this.navigate.bind(this)}>featured</button>
-			</div>
-		);
-	}
+  render() {
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+    console.log("layout");
+    return (
+      <div>
+
+        <Nav location={location} />
+
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+              <h1>KillerNews.net</h1>
+
+              {this.props.children}
+
+            </div>
+          </div>
+          <Footer/>
+        </div>
+      </div>
+
+    );
+  }
 }
